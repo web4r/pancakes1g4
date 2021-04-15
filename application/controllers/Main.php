6df22d72fa567714d36238206de89727cfac52db
web4r <websitecourse.ar@gmail.com>
2021-04-15 11:16:14 +0700

    update konfigurasi frontend

---
+++ application/controllers/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+class Main extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['frontend_page'] = "app/content";
+        $this->load->view('layouts/main', $data);
+    }
+}
